304206cecca54d8e9f178baa4cf4d44e64b08f0c
uhufor <haejung83@gmail.com>
2025-05-22 00:43:57 +0900

    add insets to activity

--- app/src/main/java/com/uhufor/inspectionsample/MainActivity.kt
+++ app/src/main/java/com/uhufor/inspectionsample/MainActivity.kt
@@ -5,11 +5,14 @@ import android.util.Log
 import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setupInsets()
         setContentView(R.layout.main_activity)
         setupViews()
     }
@@ -20,6 +23,15 @@ class MainActivity : ComponentActivity() {
         }
         findViewById<View>(R.id.clickCenter).setOnClickListener {
             Log.i("MainActivity", "Clicked Long Button")
+        }
+    }
+
+    private fun setupInsets() {
+        val mainView = findViewById<View>(android.R.id.content)
+        ViewCompat.setOnApplyWindowInsetsListener(mainView) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(insets.left, insets.top, insets.right, insets.bottom)
+            WindowInsetsCompat.CONSUMED
         }
     }
 }
