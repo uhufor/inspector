9110b8ba1e06d0fc76a8ad195300518a918eab47
uhufor <haejung83@gmail.com>
2025-05-22 00:02:59 +0900

    Make buttons can clickable

--- app/src/main/java/com/uhufor/inspectionsample/MainActivity.kt
+++ app/src/main/java/com/uhufor/inspectionsample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.uhufor.inspectionsample
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
@@ -9,5 +11,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.main_activity)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        findViewById<View>(R.id.click).setOnClickListener {
+            Log.i("MainActivity", "Clicked Button")
+        }
+        findViewById<View>(R.id.clickCenter).setOnClickListener {
+            Log.i("MainActivity", "Clicked Long Button")
+        }
     }
 }
