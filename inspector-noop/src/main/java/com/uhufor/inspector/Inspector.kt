ee24b3ccbdde46e236045a5f6ba7b4d8426d952c
uhufor <haejung83@gmail.com>
2025-06-03 00:37:29 +0900

    Add `isInspectionEnabled` property to no-op Inspector

--- inspector-noop/src/main/java/com/uhufor/inspector/Inspector.kt
+++ inspector-noop/src/main/java/com/uhufor/inspector/Inspector.kt
@@ -3,11 +3,22 @@ package com.uhufor.inspector
 import android.app.Application
 
 object Inspector {
+    var isInspectionEnabled: Boolean = false
+        private set
 
-    fun install(app: Application) {}
-    fun enableInspection() {}
-    fun disableInspection() {}
-    fun toggleInspection() {}
-    fun showFloatingTrigger() {}
-    fun hideFloatingTrigger() {}
+    fun install(app: Application) = Unit
+    fun enableInspection() {
+        isInspectionEnabled = true
+    }
+
+    fun disableInspection() {
+        isInspectionEnabled = false
+    }
+
+    fun toggleInspection() {
+        isInspectionEnabled = !isInspectionEnabled
+    }
+
+    fun showFloatingTrigger() = Unit
+    fun hideFloatingTrigger() = Unit
 }
