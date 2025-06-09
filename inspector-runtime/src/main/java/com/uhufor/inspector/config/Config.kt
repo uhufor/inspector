d946f3dc07955ed620139dc49e042b23aa8b96da
uhufor <haejung83@gmail.com>
2025-06-09 22:48:40 +0900

    feat: add traverse type to config and use it during traverse view tree

--- inspector-runtime/src/main/java/com/uhufor/inspector/config/Config.kt
+++ inspector-runtime/src/main/java/com/uhufor/inspector/config/Config.kt
@@ -1,10 +1,9 @@
 package com.uhufor.inspector.config
 
-import android.content.res.Resources
+import com.uhufor.inspector.TraverseType
 import com.uhufor.inspector.UnitMode
 
 class Config {
     var unitMode: UnitMode = UnitMode.DP
-    val densityString: String
-        get() = "%.1fx".format(Resources.getSystem().displayMetrics.density)
+    var traverseType: TraverseType = TraverseType.HIERARCHICAL
 }
