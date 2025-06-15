4554434280f86f04884fd8e6b6de18e79e41683e
uhufor <haejung83@gmail.com>
2025-06-15 23:24:46 +0900

    feat: make config as flow

--- inspector-runtime/src/main/java/com/uhufor/inspector/config/Config.kt
+++ inspector-runtime/src/main/java/com/uhufor/inspector/config/Config.kt
@@ -2,8 +2,23 @@ package com.uhufor.inspector.config
 
 import com.uhufor.inspector.TraverseType
 import com.uhufor.inspector.UnitMode
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class Config {
-    var unitMode: UnitMode = UnitMode.DP
-    var traverseType: TraverseType = TraverseType.HIERARCHICAL
+    private val _unitModeFlow = MutableStateFlow<UnitMode>(UnitMode.DP)
+    val unitModeFlow = _unitModeFlow.asStateFlow()
+    var unitMode: UnitMode
+        get() = _unitModeFlow.value
+        set(value) {
+            _unitModeFlow.value = value
+        }
+
+    private val _traverseType = MutableStateFlow<TraverseType>(TraverseType.HIERARCHICAL)
+    val traverseTypeFlow = _traverseType.asStateFlow()
+    var traverseType: TraverseType
+        get() = _traverseType.value
+        set(value) {
+            _traverseType.value = value
+        }
 }
